1539fb0adebd9cdb98aa0ae3fbb6289df0af6229
Jean Borges <jean.angelo.borges@gmail.com>
2022-10-16 17:57:32 -0300

    version 5

--- pages/home/cards.js
+++ pages/home/cards.js
@@ -1,119 +1,143 @@
-/**
-        <section class="back-itens marg-bottom">
-            <section class="flex colum gap2 ">
-              <h3>Pessoa desenvolvedora front-end - React</h3>
-              <div class="flex gap2">
-                <p>Kenzie</p>
-                <p>Curitiba</p>
-              </div>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-            </section>
-            <div class="flex between marg-top">
-                <button class="tipo-vaga">Home office</button>
-                <button id="butt-candidatar" class="candidatar">Candidatar</button>
-            </div>
-        </section>
- */
+/**        <!-- <li class="card-1_li flex_column">
+          <h2 class="card-1_li_title title-4">
+            Pessoa desenvolvedora front-end - React JS
+          </h2>
+          <div class="card-1_li_div flex_row gap-1">
+            <span class="card-1_li_div_span-1 text-3">Kenzie Academy</span>
+            <span class="card-1_li_div_span-1 text-3">Curitiba</span>
+          </div>
+          <p class="card-1_li_div_p text-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          <div class="section-1_desktop">
+            <span class="card-1_li_div_span-2 text-3">Home Office</span>
+            <button class="card-1_li_div_button button border_none button_add_card">Candidatar</button>
+          </div>
+        </li> --> */
 
-function render(arr){
-  const ulCard =  document.getElementById('vagas-tela')
+const asideJobs = []
 
-  ulCard.innerHTML = ''
+function createMainList(){
+    let jsonData = JSON.parse(localStorage.getItem("appliedJobs")) || []
+    const mainListFull = document.querySelector('.card-1_ul')
+    const asideListFull = document.querySelector('.card-2_ul')   
 
-  arr.forEach(element => {
-  const card = creatCard(element)
+    jobsData.forEach(job => { 
+    const liItens = document.createElement('li') 
+    const h2Itens = document.createElement('h2') 
+    const divItens = document.createElement('div') 
+    const spanItens = document.createElement('span') 
+    const spanItens2 = document.createElement('span')  
+    const pItens = document.createElement('p') 
+    const divItens2 = document.createElement('div')
+    const spanItensSup2 = document.createElement('span') 
+    const bttItens = document.createElement('button')
+    
+ 
+   
+    liItens.classList = 'card-1_li flex_column'
+    h2Itens.classList = 'card-1_li_title title-4' 
+    divItens.classList = 'card-1_li_div flex_row gap-1'
+    spanItens.classList = 'card-1_li_div_span-1 text-3' 
+    spanItens2.classList = 'card-1_li_div_span-1 text-3'
+    pItens.classList = 'card-1_li_div_p text-2'
+    divItens2.classList = 'section-1_desktop'
+    spanItensSup2.classList = 'card-1_li_div_span-2 text-3'
+    bttItens.classList = 'card-1_li_div_button button border_none button_add_card'
 
-  ulCard.appendChild(card)
-  })
-}
+    h2Itens.innerText = job.title
+    spanItens.innerText = job.enterprise
+    spanItens2.innerText = job.location
+    pItens.innerText = job.description
+    spanItensSup2.innerText = job.modalities[0]
+    if(jsonData.filter(element => element.title == job.title).length > 0){
+        bttItens.innerText = 'Retirar candidatura'
+    }else{
+        bttItens.innerText = 'Candidatar'
+    }
+    
 
-
-function creatCard(element){
-  const secCard = document.createElement('li')
-  const sec2Card = document.createElement('section')
-  const h3Card = document.createElement('h3')
-  const divCard = document.createElement('div')
-  const empCard = document.createElement('p')
-  const citCard = document.createElement('p')
-  const descCard = document.createElement('p')
-  const div2Card = document.createElement('div')
-  const bttTipCard = document.createElement('button')
-  const candidCard = document.createElement('button')
-
-  secCard.classList = 'back-itens  marg-bottom'
-  
-
-  sec2Card.classList = 'flex colum gap2'
-
-  h3Card.innerText = element.title
-
-  divCard.classList = 'flex gap2'
-
-  empCard.innerText = element.enterprise
-  citCard.innerText = element.location
-
-  descCard.innerText = element.descrition
-
-  div2Card.classList = 'flex between marg-top'
-
-  bttTipCard.classList.add('tipo-vaga')
-  bttTipCard.innerText = element.modalities
-
-  candidCard.classList.add('candidatar')
-  candidCard.innerText = 'Candidatar'
-  candidCard.id = element.id
-
-  divCard.append(empCard, citCard)
-  div2Card.append(bttTipCard, candidCard)
-  sec2Card.append(h3Card, divCard, descCard)
-  secCard.append(sec2Card, div2Card)
-
-  
- return secCard
-}
-
-
+    bttItens.addEventListener('click', function(){ 
         
-render(jobsData)
+        if(bttItens.innerText == 'Candidatar'){ 
+            asideListFull.innerHTML = '' 
+            bttItens.innerText = 'Retirar candidatura'
+            let newObject = {
+                id:job.id, 
+                title:job.title, 
+                enterprise:job.enterprise, 
+                location:job.location
+            }
+            asideJobs.push(newObject)
+            localStorage.setItem("appliedJobs", JSON.stringify(asideJobs))
+            renderAside(asideJobs) 
+        }else{
+            asideListFull.innerHTML = ''
+            bttItens.innerText = 'Candidatar' 
+            let findIndexAside = asideJobs.indexOf(job)
+            asideJobs.splice(findIndexAside, 1)
+            localStorage.setItem("appliedJobs", JSON.stringify(asideJobs))
+            renderAside(asideJobs)
+        }
+    })
 
-
-/*cards favoritos
-        <section class="novos-selec start">
-            <div class="flex between gap">
-              <h3>Pessoa desenvolvedora front-end - React JS</h3>
-              <button class="trash"><img src="./assets/img/trash.svg" alt=""></button>
-            </div>
-            <div class="flex gap2 marg-top">
-              <p>Kenzie</p>
-              <p>Curitiba</p>
-            </div>
-        </section>*/
-
-function cardsFavort(element){
-    const cfSection = document.createElement('section')
-    const cfDiv = document.createElement('div')
-    const cfh3 = document.createElement('h3')
-    const cfButton = document.createElement('button')
-    const cfDiv2 = document.createElement('div')
-    const cfCurso = document.createElement('p')
-    const cfCit = document.createElement('p')
-
-    cfSection.classList = 'novos-selec cardVagas start'
-    cfDiv.classList = 'flex between gap'
-    cfh3.innerText = element.title
-
-    cfButton.classList.add('trash')
-    cfButton.src = "./assets/img/trash.svg"
-
-    cfDiv2.classList = 'flex gap2 marg-top'
-    cfCurso.innerText = element.enterprise
-    cfCit.innerText = element.location
-
-    cfDiv.append(cfh3, cfButton)
-    cfDiv2.append(cfCurso, cfCit)
-    cfSection.append(cfDiv, cfDiv2)
-
-    return cfSection
+    divItens2.append(spanItensSup2, bttItens)
+    divItens.append(spanItens, spanItens2)
+    liItens.append(h2Itens, divItens, pItens, divItens2)
+    mainListFull.appendChild(liItens)
+    });
+    return mainListFull
 }
 
+/**       <!-- <li class="card-2_li flex_column">
+          <div class="card-2_div flex_row align_center justify_between">
+            <h2 class="card-2_li_title title-5">Pessoa desenvolvedora front-end - React JS</h2>
+            <button class="card-2_li_button"><img src="../../assets/img/trash.svg" alt=""></button>
+          </div>
+          <div class="card-2_div-2 flex_row gap-1">
+            <span class="card-2_span text-3">Kenzie Academy</span>
+            <span class="card-2_span text-3">Curitiba</span>
+          </div>        
+        </li> --> */
+function createAside(asideList){
+    const asideListFull = document.querySelector('.card-2_ul')
+    const mainListFull = document.querySelector('.card-1_ul')
+    asideList.forEach(job => {
+        const asideItens = document.createElement('li')
+        const asideItensShow = document.createElement('div')
+        const asideItensShowTitle = document.createElement('h2')
+        const asideItensShowBtn = document.createElement('button')
+        const asideItensShowBtnImg = document.createElement('img')
+        const asideItensDivTwo = document.createElement('div')
+        const asideItensDivTwoSpanOne = document.createElement('span')
+        const asideItensDivTwoSpanTwo = document.createElement('span')
 
+        asideItens.classList = 'card-2_li flex_column'
+        asideItensShow.classList = 'card-2_div flex_row align_center justify_between'
+        asideItensShowTitle.classList = 'card-2_li_title title-5'
+        asideItensShowBtn.classList = 'card-2_li_button'
+        asideItensDivTwo.classList = 'card-2_div-2 flex_row gap-1'
+        asideItensDivTwoSpanOne.classList = 'card-2_span text-3'
+        asideItensDivTwoSpanTwo.classList = 'card-2_span text-3'
+
+        asideItensShowTitle.innerText = job.title
+        asideItensDivTwoSpanOne.innerText = job.enterprise
+        asideItensDivTwoSpanTwo.innerText = job.location
+        asideItensShowBtnImg.src = '../../assets/img/trash.svg'
+
+        asideItensShowBtn.addEventListener('click', function(){
+            asideListFull.innerHTML = ''
+            mainListFull.innerHTML = ''
+            let findIndexAside = asideJobs.indexOf(job)
+            asideJobs.splice(findIndexAside, 1)
+            localStorage.setItem("appliedJobs", JSON.stringify(asideJobs))
+            renderMainList()
+            renderAside(asideJobs)    
+        })
+
+        asideItensDivTwo.append(asideItensDivTwoSpanOne, asideItensDivTwoSpanTwo)
+        asideItensShowBtn.appendChild(asideItensShowBtnImg)
+        asideItensShow.append(asideItensShowTitle, asideItensShowBtn)
+        asideItens.append(asideItensShow, asideItensDivTwo)
+        asideListFull.appendChild(asideItens)
+    })
+    return asideListFull
+}
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,41 +1,34 @@
-/* Desenvolva sua lógica aqui... */
-let id = 0
-/*
-function favoritsCards(){
-    const buttonFavorit = document.querySelectorAll('.candidatar')
-    const secFavorit = document.getElementById('favorit')
-    
-    buttonFavorit.addEventLinester('click', (event) => {
-        event.preventDefault()
-        const favcards = cardsFavort(jobsData)
-        secFavorit.appendChild(favcards)
+function createAsideEmpty(){ 
+    const asideList = document.querySelector('.card-2_ul')
+    const asideItens = document.createElement('div') 
+    const infoAside = document.createElement('p')
 
-    })
-    
+    infoAside.innerText = 'Você ainda não aplicou para nenhuma vaga!'
+    asideItens.appendChild(infoAside)
+    asideList.appendChild(asideItens)
+    return asideList
 }
-favoritsCards()*/
 
-
-function showFavCards(data){
-    const buttonFav = document.querySelectorAll('.candidatar')
-   
-    
-    buttonFav.forEach((button, index)=> {
-        buttonFav.addEventLinester('click', (event) =>{
-            const itensVaga = document.querySelectorAll('.favorit')
-            const itensJson = JSON.stringify(data[index])
-            if(button.innerText == "Remover Candidatura"){
-                itensVaga.remove()
-                localStorage.removeItem('itens', itensJson)
-                button.innerText = 'Candidatar'
-            }else{
-                itensVaga.classList.toggle('cardVagas')
-                button.innerText = 'Remover Candidatura'
-                localStorage.setItem('itens', itensJson)
-            }
-
-    })
-
-    })
+function renderMainList(){
+    const mainSection = document.querySelector('#sectionOne')
+    return mainSection.appendChild(createMainList())
 }
-showFavCards(jobsData)
+
+
+function renderAside(list){
+    const asideSection = document.querySelector('.card-2')
+    if(list.length == 0){
+        return asideSection.appendChild(createAsideEmpty())
+    }else{
+        return asideSection.appendChild(createAside(list))
+    }
+}
+
+function getJsonItem(){
+    let getItem = JSON.parse(localStorage.getItem("appliedJobs")) || []
+    getItem.forEach(element => asideJobs.push(element))
+    return asideJobs
+}
+
+renderAside(getJsonItem())
+renderMainList()
